bf9087efd1e7c86feeb24dd865648adfba8bda70
yarakaddoura <ykaddoura@aus.edu>
2023-01-31 04:42:38 +0400

    Added feature B

--- src/project1/Project1.java
+++ src/project1/Project1.java
@@ -1,10 +1,13 @@
 package project1;
 
 public class Project1 {
-
+	public static void featureB() {
+		System.out.println("Feature B");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		featureB();
 	}
 
 }
